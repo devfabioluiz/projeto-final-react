278a20da8a752f23564b3735545a81230c4c1d45
Fabio Luiz <devfabioluiz@gmail.com>
2025-06-25 22:14:38 -0300

    meu primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
 
-function App() {
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import Dashboard from "./components/Dashboard";
+import Settings from "./components/Settings";
+import Profile from "./components/Profile"
 
+const App = () => {
   return (
-    <>
-      Meu app em react
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={ <Dashboard />}>
+          <Route path="profile" element={ <Profile />} />
+          <Route path="settings" element={ <Settings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Settings.jsx
@@ -0,0 +1,7 @@
+// Crie um arquivo chamado Settings.jsx
+const Settings = () => {
+  return <h1>Configurações</h1>
+}
+
+export default Settings;
+    
