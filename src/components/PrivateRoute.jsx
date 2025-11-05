ca1c9e87edca038ed5f6a4db27b5c09356cfa631
Fabio Luiz Daemon Da Silva <devfabioluiz@gmail.com>
2025-11-05 19:26:23 -0300

    .

--- src/components/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-//Crie um arquivo chamado PrivateRoute.jsx
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = true; // Simulação de autenticação
-  return isAuthenticated ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
--- src/components/pages/Settings.jsx
+++
@@ -1,7 +0,0 @@
-// Crie um arquivo chamado Settings.jsx
-const Settings = () => {
-  const usuario = "Usuário";
-  return <h1>Configurações de {usuario}</h1>;
-};
-
-export default Settings;
